4a85b34963bbf03cefd1ded3f4c4189ab0788364
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2022-05-07 20:39:54 +0900

    test compare_nat_2/3_2

---
+++ input/compare_nat/compare_nat2_2.ml
@@ -0,0 +1,8 @@
+open System.Compare_nat2
+open Compare_nat2
+
+let () =
+  let judgement = J_less (S (S Z), S (S (S (S (S Z))))) in
+  let tree = derive judgement in
+  let s = Tree.string_of_tree tree in
+  print_endline s
