ab4ea1b4fba027493dcc60c5c2fda7d133bc0dbf
Junya Hirashima <hirashima.junya.34z@st.kyoto-u.ac.jp>
2022-06-09 12:41:35 +0900

    re-impl nat with inference

--- src/system/nat.ml
+++ src/system/nat.ml
@@ -1,14 +1,38 @@
 module System = struct
-  type nat = Z | S of nat
+  let fresh_int =
+    let counter = ref ~-1 in
+    let body () =
+      counter := !counter + 1 ;
+      !counter in
+    body
+
+  type nat = Z | S of nat | V of int
   type op = Plus | Times
   type bin_op = BinOp of op * nat * nat
   type judgement = J_is of bin_op * nat
   type rule = P_Zero | P_Succ | T_Zero | T_Succ
 
+  let inversion = function
+    | J_is (BinOp (Plus, Z, n), n') -> (P_Zero, [], [(n, n')])
+    | J_is (BinOp (Plus, S n1, n2), n') ->
+        let n = V (fresh_int ()) in
+        (P_Succ, [J_is (BinOp (Plus, n1, n2), n)], [(S n, n')])
+    | J_is (BinOp (Times, Z, _), n') -> (T_Zero, [], [(n', Z)])
+    | J_is (BinOp (Times, S n1, n2), n4) ->
+        let n3 = V (fresh_int ()) in
+        ( T_Succ
+        , [J_is (BinOp (Times, n1, n2), n3); J_is (BinOp (Plus, n2, n3), n4)]
+        , [] )
+    | J_is (BinOp ((Plus | Times), V _, _), _) -> assert false
+
   let result_of_judgement = function J_is (_, n) -> n
 
   module String = struct
-    let rec of_nat = function Z -> "Z" | S n -> "S(" ^ of_nat n ^ ")"
+    let rec of_nat = function
+      | Z -> "Z"
+      | S n -> "S(" ^ of_nat n ^ ")"
+      | V i -> "n" ^ string_of_int i
+
     let of_op = function Plus -> "plus" | Times -> "times"
 
     let of_bin_op = function
@@ -32,19 +56,69 @@ module Tree = Core.Derivation_tree.Make (System)
 open System
 open Tree
 
-let rec derive_bin_op bin_op =
-  match bin_op with
-  | BinOp (Plus, Z, n) -> Tree (J_is (bin_op, n), P_Zero, [])
-  | BinOp (Plus, S n1, n2) ->
-      let t = derive_bin_op (BinOp (Plus, n1, n2)) in
-      let n = result_of_judgement (root_of_tree t) in
-      Tree (J_is (bin_op, S n), P_Succ, [t])
-  | BinOp (Times, Z, _) -> Tree (J_is (bin_op, Z), T_Zero, [])
-  | BinOp (Times, S n1, n2) ->
-      let t1 = derive_bin_op (BinOp (Times, n1, n2)) in
-      let n3 = result_of_judgement (root_of_tree t1) in
-      let t2 = derive_bin_op (BinOp (Plus, n2, n3)) in
-      let n4 = result_of_judgement (root_of_tree t2) in
-      Tree (J_is (bin_op, n4), T_Succ, [t1; t2])
+type subst = (int * nat) list
 
-let derive = function J_is (bin_op, _) -> derive_bin_op bin_op
+let rec one_subst_nat (i, n) = function
+  | Z -> Z
+  | S n' -> S (one_subst_nat (i, n) n')
+  | V i' -> if i = i' then n else V i'
+
+let subst_nat subst nat =
+  List.fold_left (fun nat' s -> one_subst_nat s nat') nat subst
+
+let one_subst_judgement s = function
+  | J_is (BinOp (op, n1, n2), n3) ->
+      let n1 = one_subst_nat s n1 in
+      let n2 = one_subst_nat s n2 in
+      let n3 = one_subst_nat s n3 in
+      J_is (BinOp (op, n1, n2), n3)
+
+let subst_judgement subst judgement =
+  List.fold_left (fun ty' s -> one_subst_judgement s ty') judgement subst
+
+let rec one_subst_tree s = function
+  | Tree (j, r, st) ->
+      Tree (one_subst_judgement s j, r, List.map (one_subst_tree s) st)
+
+let subst_tree subst tree =
+  List.fold_left (fun ty' s -> one_subst_tree s ty') tree subst
+
+let eqs_of_subst subst = List.rev_map (fun (i, n) -> (V i, n)) subst
+
+let rec subst_eqs subst eqs =
+  match subst with
+  | [] -> eqs
+  | s :: rest ->
+      let f n = one_subst_nat s n in
+      let eqs' = List.rev_map (fun (n1, n2) -> (f n1, f n2)) eqs in
+      subst_eqs rest eqs'
+
+let rec of_nat = function
+  | Z -> "Z"
+  | S n -> "S(" ^ of_nat n ^ ")"
+  | V i -> "n" ^ string_of_int i
+
+let rec unify = function
+  | [] -> []
+  | (n, n') :: eqs -> (
+      if n = n' then unify eqs
+      else
+        match (n, n') with
+        | S n, S n' -> unify ((n, n') :: eqs)
+        | n, V i | V i, n -> (i, n) :: unify (subst_eqs [(i, n)] eqs)
+        | _ -> assert false )
+    
+
+let rec derive judgement =
+  let rule, sub_judgement_list, eqs = inversion judgement in
+  let rec aux subst = function
+    | [] -> ([], subst)
+    | j' :: sj' -> (
+        let tree, subst = derive (subst_judgement subst j') in
+        match aux subst sj' with ts, subst -> (tree :: ts, subst) ) in
+  let sub_tree_list, subst = aux [] sub_judgement_list in
+  let eqs = eqs @ eqs_of_subst subst in
+  let subst = unify eqs in
+  (Tree (subst_judgement subst judgement, rule, sub_tree_list), subst)
+
+let derive j = match derive j with t, _ -> t
